3982d6178b1a15f3fea164174e58cc0cec3e74d0
Federico Juretich <fedejuret@gmail.com>
2022-07-28 20:53:57 -0300

    add routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,57 @@
 const express = require('express');
 const session = require('express-session');
+const cookie = require('cookie-parser');
 
 const app = express();
+
+app.use(session({
+    secret: '123456789', // Pueden poner cualquier cosa
+    cookie: {
+        expires: false
+    },
+    saveUninitialized: true,
+    resave: false
+}));
+app.use(cookie());
+
+
+app.get('/cookies', (req, res) => {
+
+    return res.json({
+        cookies: req.cookies
+    })
+
+});
+
+app.get('/set-cookie', (req, res) => {
+
+    const { cookie } = req.query;
+
+    res.cookie('30-seconds-cookie', cookie, {
+        maxAge: 1000 * 30
+    });
+
+    return res.send(cookie);
+
+});
+
+app.get('/sessions', (req, res) => {
+
+    return res.json({
+        sessions: req.session
+    })
+
+});
+
+app.get('/set-session', (req, res) => {
+
+    const { session } = req.query;
+
+    req.session.hola = session;
+
+    return res.send(session);
+
+});
 
 
 
